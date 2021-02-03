4c480938c55db40a73c4d3337de48998f9d6019b
vahidbe <vahid.beyraghi@student.uclouvain.be>
2021-02-03 17:14:44 +0100

    update config insects

--- samples/insects/insects.py
+++ samples/insects/insects.py
@@ -73,7 +73,7 @@ class InsectConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + toy
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 1
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
